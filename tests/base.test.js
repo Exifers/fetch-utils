6c6ceeee30943b302d36744a03bcb37c5a3da0e2
Emmanuel <meric.emmanuel@gmail.com>
2019-10-05 16:56:13 +0200

    added tests

--- tests/base.test.js
+++ tests/base.test.js
@@ -1,5 +1,5 @@
 import fetch from 'jest-fetch-mock'
-import { fetchBase } from '../src/base'
+import { fetchBase, fetchBaseBody } from '../src/base'
 
 test('fetchBase returns the response from fetch call', (done) => {
   const response = { someKey: 'someValue' }
@@ -29,7 +29,10 @@ test('fetchBase sets csrf header default name', (done) => {
     }))
   })
 
-  fetchBase('/', {}, { customFetch })
+  fetchBase('/', {}, {
+    customFetch,
+    insertCsrfToken: true
+  })
     .then(() => {
       expect(customFetch.mock.calls.length).toBe(1)
       expect(customFetch.mock.calls[0][1]['headers']['X-CSRFToken']).toEqual(
@@ -51,7 +54,8 @@ test('fetchBase sets given csrf header name', (done) => {
 
   fetchBase('/', {}, {
     customFetch,
-    csrfHeaderName
+    csrfHeaderName,
+    insertCsrfToken: true
   })
     .then(() => {
       expect(customFetch.mock.calls.length).toBe(1)
@@ -74,7 +78,8 @@ test('fetchBase sets given csrf header value', (done) => {
 
   fetchBase('/', {}, {
     customFetch,
-    csrfHeaderValue
+    csrfHeaderValue,
+    insertCsrfToken: true
   })
     .then(() => {
       expect(customFetch.mock.calls.length).toBe(1)
@@ -97,7 +102,8 @@ test('fetchBase sets given csrf header value as a function', (done) => {
 
   fetchBase('/', {}, {
     customFetch,
-    csrfHeaderValue
+    csrfHeaderValue,
+    insertCsrfToken: true
   })
     .then(() => {
       expect(customFetch.mock.calls.length).toBe(1)
@@ -108,4 +114,102 @@ test('fetchBase sets given csrf header value as a function', (done) => {
     })
 })
 
+test('fetchBase inserts given content type', (done) => {
+  const customFetch = jest.fn(() => {
+    return new Promise((res) => res({
+      ok: true,
+      json: () => new Promise((res) => res({}))
+    }))
+  })
 
+  const insertContentType = true
+  const contentTypeHeaderValue = 'multipart/form-data'
+
+  fetchBase('/', {}, {
+    customFetch,
+    insertContentType,
+    contentTypeHeaderValue
+  })
+    .then(() => {
+      expect(customFetch.mock.calls.length).toBe(1)
+      expect(customFetch.mock.calls[0][1]['headers']['Content-Type']).toEqual(
+        contentTypeHeaderValue
+      )
+      done()
+    })
+})
+
+test('fetchBase doesn\'t insert content type header by default', (done) => {
+  const customFetch = jest.fn(() => {
+    return new Promise((res) => res({
+      ok: true,
+      json: () => new Promise((res) => res({}))
+    }))
+  })
+
+  fetchBase('/', {}, {
+    customFetch
+  })
+    .then(() => {
+      expect(customFetch.mock.calls.length).toBe(1)
+      expect(customFetch.mock.calls[0][1]['headers']['Content-Type']).toEqual(
+        undefined
+      )
+      done()
+    })
+})
+
+test('fetchBase doesn\'t insert csrf token header by default', (done) => {
+  const customFetch = jest.fn(() => {
+    return new Promise((res) => res({
+      ok: true,
+      json: () => new Promise((res) => res({}))
+    }))
+  })
+
+  const csrfHeaderName = 'X-CSRFToken'
+
+  fetchBase('/', {}, {
+    customFetch,
+    csrfHeaderName
+  })
+    .then(() => {
+      expect(customFetch.mock.calls.length).toBe(1)
+      expect(customFetch.mock.calls[0][1]['headers'][csrfHeaderName]).toEqual(
+        undefined
+      )
+      done()
+    })
+})
+
+test('fetchBaseBody handles object input as a json request', () => {
+  const customFetchBase = jest.fn(() => {
+  })
+
+  const body = {
+    a: 1,
+    b: 2
+  }
+
+  fetchBaseBody('/', body, {}, { customFetchBase })
+
+  expect(customFetchBase.mock.calls.length).toBe(1)
+  expect(customFetchBase.mock.calls[0][1]['body']).toBe(JSON.stringify(body))
+  expect(customFetchBase.mock.calls[0][2]['contentTypeHeaderValue']).toBe('application/json')
+})
+
+test('fetchBaseBody handles formData input as a formData request', () => {
+  const customFetchBase = jest.fn(() => {
+  })
+
+  let body = new FormData()
+  body.append('a', '1')
+  body.append('b', '2')
+
+  fetchBaseBody('/', body, {}, { customFetchBase })
+
+  expect(customFetchBase.mock.calls.length).toBe(1)
+  expect(customFetchBase.mock.calls[0][1]['body']).toBe(body)
+  expect(customFetchBase.mock.calls[0][2]['insertContentType']).toBe(false)
+  expect(customFetchBase.mock.calls[0][2]['contentTypeHeaderValue']).toBe(undefined)
+})
